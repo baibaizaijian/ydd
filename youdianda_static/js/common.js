a1106c78eadbd08e5eb08e6e9149add89553a6f7
bzz <3024131925@qq.com>
2023-08-15 22:44:26 +0800

    删除分类完成

--- youdianda_static/js/common.js
+++ youdianda_static/js/common.js
@@ -10,7 +10,7 @@ axios.defaults.baseURL = 'http://124.223.14.236:8060/admin'
 
 axios.interceptors.request.use(
   (config) => {
-    config.headers['x-token'] = localStorage.getItem('token')
+    config.headers['x-token'] = localStorage.getItem('ydd-token')
     return config
   },
   (e) => Promise.reject(e)
@@ -20,10 +20,10 @@ axios.interceptors.response.use(
   // 直接拿到数据,数据脱壳
   (res) => res.data, // http响应状态码为2xx和3xx进入第一个函数
   (e) => { // http响应状态码为4xx和5xx, 进入第二个函数
-    if (e.response.status === 401) {
-      localStorage.clear()
-      location.href = './login.html'
-    }
+    // if (e.response.status === 401) {
+    //   localStorage.clear()
+    //   location.href = './login.html'
+    // }
     return Promise.reject(e)
   }
 )
